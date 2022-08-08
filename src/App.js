a9ccc2f7285602bbdfa9aed90bee20616344ab2f
Sirui Zhang <siruizhangdev@gmail.com>
2022-08-08 17:58:25 -0400

    migrate user context and categories context to redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import {useEffect} from 'react'
+import {useDispatch} from "react-redux";
+
 import {Routes, Route} from "react-router-dom";
+
+import {
+  createUserDocumentFromAuth, getCategoriesAndDocuments,
+  onAuthStateChangedListener
+} from "./utils/firebase/firebase.utils";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/nagivation/navigation.component";
 import AuthenticationComponent from "./routes/authentication/authentication.component";
 import Shop from "./routes/shop/shop.component";
 import Checkout from "./routes/checkout/checkout.component";
+import {setCurrentUser} from "./store/user/user.action";
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    // unsubscribe when it unmounts
+    const unsubscrib = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      // this state change will cause react to rerender this component
+      // therefore rerender its child component
+      dispatch(setCurrentUser(user))
+    })
+
+    return unsubscrib
+  }, [])
+
   return (
     <Routes>
       <Route path={'/'} element={<Navigation/>}>
--- src/routes/nagivation/navigation.component.jsx
+++ src/routes/nagivation/navigation.component.jsx
@@ -1,15 +1,25 @@
-import {Outlet} from "react-router-dom";
 import {Fragment, useContext} from "react";
-import {ReactComponent as CrownLogo} from "../../assets/crown.svg";
+import {Outlet} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectCurrentUser} from "../../store/user/user.selector";
+
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
-import {signOutUser} from "../../utils/firebase/firebase.utils";
-import {UserContext} from "../../contexts/user.context";
+
 import {CartContext} from "../../contexts/cart.context";
-import {NavigationContainer, LogoContainer, NavLinks, NavLink} from "./navigation.styles"
+
+import {ReactComponent as CrownLogo} from "../../assets/crown.svg";
+import {signOutUser} from "../../utils/firebase/firebase.utils";
+
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink
+} from "./navigation.styles"
 
 const Navigation = () => {
-  const {currentUser} = useContext(UserContext)
+  const currentUser = useSelector(selectCurrentUser)
   const {isCartOpen} = useContext(CartContext)
 
   return (
