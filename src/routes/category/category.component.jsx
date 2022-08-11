46149bb90a7b8816f40dba3b1a762bfce4497bb4
Sirui Zhang <siruizhangdev@gmail.com>
2022-08-10 22:37:08 -0400

    add redux-thunk and spinner component, refactore setCategories into fetchCategoriesAsync thunk

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -5,11 +5,13 @@ import {useState, useEffect, Fragment} from "react";
 import React from 'react';
 import ProductCard from "../../components/product-card/product-card.component";
 import {useSelector} from "react-redux";
-import {selectCategoriesMap} from "../../store/categories/categories.selector";
+import {selectCategoriesIsLoading, selectCategoriesMap} from "../../store/categories/categories.selector";
+import Spinner from "../../components/spinner/spinner.component";
 
 const Category = () => {
   const {category} = useParams()
   const categoriesMap = useSelector(selectCategoriesMap)
+  const isLoading = useSelector(selectCategoriesIsLoading)
   const [products, setProducts] = useState(categoriesMap[category])
 
   useEffect(() => {
@@ -19,12 +21,13 @@ const Category = () => {
   return (
     <Fragment>
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {products &&
-          products.map((product) =>
-            <ProductCard key={product.id} product={product}/>
-          )}
-      </CategoryContainer>
+      {isLoading ? <Spinner/> :
+        <CategoryContainer>
+          {products &&
+            products.map((product) =>
+              <ProductCard key={product.id} product={product}/>
+            )}
+        </CategoryContainer>}
     </Fragment>
   );
 };
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,19 +2,14 @@ import {Routes, Route} from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
 import {useEffect} from "react";
-import {setCategories} from "../../store/categories/categories.action";
+import {fetchCategoriesAsync} from "../../store/categories/categories.action";
 import {useDispatch} from "react-redux";
-import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils";
 
 const Shop = () => {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    const getCategoriesArray = async () => {
-      const categoriesArray = await getCategoriesAndDocuments()
-      dispatch(setCategories(categoriesArray))
-    }
-    getCategoriesArray()
+    dispatch(fetchCategoriesAsync())
   }, [])
 
   return (
